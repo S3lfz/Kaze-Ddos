8b0e25e364144978120294cf1a34d270db4f949a
intosmoke <exxtasiss9@gmail.com>
2025-11-03 19:38:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ async def on_ready(): print(f'On {bt.user}')
 @bt.tree.command(name='404')
 async def spam(i: discord.Interaction):
     await i.response.send_message("intosmoke", ephemeral=True)
-    await i.followup.send("[/404 Pwned This](https://discord.gg/5WbtZedcwt)")
-    await asyncio.sleep(0.1)
+    for _ in range(20):
+        await i.followup.send("[/404 Pwned This](https://discord.gg/5WbtZedcwt)")
+        await asyncio.sleep(0.1)
 
 bt.run("") #Your bot token
